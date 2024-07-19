b671237d18ffc5b07cc74d682a0e51881a06d124
Hemslo Wang <dwan@redhat.com>
2024-07-19 14:19:53 +1000

    Reorder buffer-size parameter (#120)
    Sync from https://gitlab.com/glitchtip/glitchtip-backend/-/merge_requests/881
    
    Signed-off-by: Di Wang <dwan@redhat.com>

--- bin/run-uwsgi.sh
+++ bin/run-uwsgi.sh
@@ -18,6 +18,7 @@ exec uwsgi \
     --module=glitchtip.wsgi:application \
     --env DJANGO_SETTINGS_MODULE=glitchtip.settings \
     --master --pidfile=/tmp/project-master.pid \
+    --buffer-size=83146 \
     --log-x-forwarded-for \
     --log-format-strftime \
     --http=$HTTP_SOCKET \
@@ -38,7 +39,6 @@ exec uwsgi \
     --enable-threads \
     --single-interpreter \
     --post-buffering \
-    --buffer-size=83146 \
     --ignore-sigpipe \
     --ignore-write-errors \
     --disable-write-exception \
